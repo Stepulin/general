a03fe9477989fbd9314ce94585c06704481b08fa
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-09-16 23:14:15 +0200

    Create package.sh

---
+++ package/package.sh
@@ -0,0 +1,22 @@
+# Preparing massive package; however, still in progress
+
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            bash script.sh
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
