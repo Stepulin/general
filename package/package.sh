6e2408881e392b6e205a12b1e2cd78a7e74ff673
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-09-20 19:40:14 +0200

    Update package.sh

--- package/package.sh
+++ package/package.sh
@@ -5,14 +5,14 @@ options=("Option 1" "Option 2" "Option 3" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
+        "U P D A T E")
             wget https://raw.githubusercontent.com/Stepulin/general/master/package/update.sh && bash update.sh
             ;;
-        "Option 2")
-            echo "you chose choice 2"
+        " F I R E W A L L")
+            rm firewall* && wget https://raw.githubusercontent.com/Stepulin/general/master/firewall/firewall_apply.sh && wget https://raw.githubusercontent.com/Stepulin/general/master/firewall/firewall_rules.sh && bash firewall_apply.sh
             ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
+        " - - - -")
+            echo "Nothing in there yet"
             ;;
         "Quit")
             break
