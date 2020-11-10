0644a8c0d488f725afef903cb110b870107d9a47
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-10 20:30:58 +0100

    Update ipvX.sh

--- firewall/ipvX.sh
+++ firewall/ipvX.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # The main purpose of this script is to use it along with OpenVPN
 # It allows forwarding ipv4 and disables ipv6
+# To revert these changes, comment out these lines at the end of file /etc/sysctl.conf
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "Finalising ..."
 echo "DONE!"
+echo "You can apply changes by running this command: sysctl -p"
 # This is Debian based, for Red Hat, us following
 # To DISABLE
 #sysctl -w net.ipv6.conf.all.disable_ipv6=1
@@ -15,5 +17,5 @@ echo "DONE!"
 #sysctl -w net.ipv6.conf.all.disable_ipv6=0
 #sysctl -w net.ipv6.conf.default.disable_ipv6=0
 echo "----------------------------"
-echo "REBOOT is highly recommended"
+echo "However REBOOT is highly recommended"
 echo "----------------------------"
