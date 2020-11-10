76f3fd53205b14bcf08cef66d7827b7b6596229a
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-10 17:36:54 +0100

    Update ssh.sh

--- package/ssh/ssh.sh
+++ package/ssh/ssh.sh
@@ -1,1 +1,17 @@
-echo
+#!/bin/bash
+echo "You SSH config should be changed for better security"
+echo "Change that ROOT and also other users cannot log in while using password"
+echo "..."
+echo "Allowing auth via cert only"
+# Find all possible lines related to root user and change it into PermitRootLogin without-password
+sed -i 's/#PermitRootLogin yes/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin no/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin no/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin without-password/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+# Disable password authentification and empty passwords (just for sure)
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
+echo "Completing ..."
