454959b9d45b6153433773224ab88f1b74240006
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-11 20:59:35 +0100

    Update client.sh

--- package/openvpn/client.sh
+++ package/openvpn/client.sh
@@ -9,3 +9,20 @@ mv client_user.ovpn $surnamefirstname.ovpn
 
 cd ..
 cp dh.pem ta.key $surnamefirstname/
+
+
+# Config only OVPN file
+cd /etc/openvpn/$surnamefirstname/
+echo "key-direction 1" >> $surnamefirstname.ovpn
+echo "<ca>" >> $surnamefirstname.ovpn
+sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' < ca.crt >> $surnamefirstname.ovpn
+echo "</ca>" >> $surnamefirstname.ovpn
+echo "<cert>" >> $surnamefirstname.ovpn
+sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' < $surnamefirstname.crt >> $surnamefirstname.ovpn
+echo "</cert>" >> $surnamefirstname.ovpn
+echo "<key>" >> $surnamefirstname.ovpn
+sed -n '/BEGIN PRIVATE KEY/,/END PRIVATE KEY/p' < $surnamefirstname.key >> $surnamefirstname.ovpn
+echo "</key>" >> $surnamefirstname.ovpn
+echo "<tls-auth>" >> $surnamefirstname.ovpn
+sed -n '/BEGIN OpenVPN Static key V1/,/END OpenVPN Static key V1/p' < ta.key >> $surnamefirstname.ovpn
+echo "</tls-auth>" >> $surnamefirstname.ovpn
