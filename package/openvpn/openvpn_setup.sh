4f1bd084e64708d8adb7116f9d74ab689ea04227
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-14 23:33:31 +0100

    Update openvpn_setup.sh

--- package/openvpn/openvpn_setup.sh
+++ package/openvpn/openvpn_setup.sh
@@ -1,9 +1,10 @@
+# Install OpenVPN
 apt install openvpn
 
+# Copy Easy-RSA and edit some lines based on your preferences
 cp -r /usr/share/easy-rsa/ /etc/openvpn/
 cd /etc/openvpn/easy-rsa/
 nano vars
-
 
 # Initialize
 ./easyrsa init-pki
@@ -15,7 +16,7 @@ nano vars
 ./easyrsa sign-req server vpnserver
 # Generate DH file
 ./easyrsa gen-dh
-# /etc/openvpn/easy-rsa/pki/dh.pem
+# /etc/openvpn/easy-rsa/pki/dh.pem ???
 # Generate extra file for safety
 openvpn --genkey --secret ta.key
 # Copy all the files so you can run the server
@@ -25,20 +26,22 @@ cp pki/private/vpnserver.key /etc/openvpn/
 cp pki/issued/vpnserver.crt /etc/openvpn/
 cp pki/dh.pem /etc/openvpn/
 
-echo "Client part"
+echo "Creating first client"
+cd /etc/openvpn/easy-rsa/
 echo "Enter you name in following form: surnamefirstname"
 read surnamefirstname
 ./easyrsa gen-req $surnamefirstname nopass
 ./easyrsa sign-req client $surnamefirstname
-
 mkdir /etc/openvpn/$surnamefirstname/
 cp pki/ca.crt /etc/openvpn/$surnamefirstname/
 cp pki/issued/$surnamefirstname.crt /etc/openvpn/$surnamefirstname/
 cp pki/private/$surnamefirstname.key /etc/openvpn/$surnamefirstname/
 
+# Remove this folders
 rm -r /etc/openvpn/server/
 rm -r /etc/openvpn/client/
 
+# Download server configuration
 cd /etc/openvpn/
 wget https://raw.githubusercontent.com/Stepulin/general/master/package/openvpn/server.conf
 
@@ -49,10 +52,14 @@ systemctl enable openvpn@server
 # To see the log
 tail -f /var/log/openvpn/openvpn.log
 
-iptables -A INPUT -p udp --dport 1194 -j ACCEPT
-iptables -A FORWARD -i eth0 -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -s 192.168.123.0/24 -o eth0 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 192.168.123.0/24 -o eth0 -j MASQUERADE
+# Add necessary rules into firewall
+echo "Adding necessary rules into firewall"
+echo "..."
 
+echo "iptables -A INPUT -p udp --dport 1194 -j ACCEPT" >> /root/firewall_rules.sh
+echo "iptables -A FORWARD -i eth0 -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT" >> /root/firewall_rules.sh
+echo "iptables -A FORWARD -s 192.168.123.0/24 -o eth0 -j ACCEPT" >> /root/firewall_rules.sh
+echo "iptables -t nat -A POSTROUTING -s 192.168.123.0/24 -o eth0 -j MASQUERADE" >> /root/firewall_rules.sh
+
+echo "Applying these rules by running firewall_apply.sh"
 bash /root/firewall_apply.sh
-
