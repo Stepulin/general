488ba8b5390d48d4d7e386464305dae64b781d90
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-14 23:19:02 +0100

    Create firewall.sh

---
+++ firewall/firewall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+echo "##################################################"
+echo "###############      Firewall      ###############"
+echo "##################################################"
+PS3='Please enter your choice: '
+optionsfirewall=("Print rules" "Download and apply" "Add rules for OpenVPN" "Exit")
+select opt in "${optionsfirewall[@]}"
+do
+    case $opt in
+        "Print rules")
+            echo "printing fw rules"
+            ;;
+        "Download and apply")
+            echo "downloading and applying"
+            ;;
+        "Add rules for OpenVPN")
+            echo "adding rules for OpenVPN"
+            ;;
+        "Exit")
+            echo "exiting"
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
