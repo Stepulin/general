13072ca64dcc91eff2095b7e4b5b306bf345e0b8
Stepulin <35203513+Stepulin@users.noreply.github.com>
2020-11-21 14:07:28 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@ echo "##################################################"
 echo "###############    script//menu    ###############"
 echo "##################################################"
 PS3='Please enter your choice: '
-options=("Exit" "Clear" "Update" "Firewall" "Clean Up")
+options=("Exit" "Clear" "Update" "Firewall" "Clean Up" "OpenVPN server")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -25,6 +25,9 @@ do
             echo "Deleting everything in this folder"
             rm *
             echo "Done"
+            ;;
+        "OpenVPN server")
+            wget https://raw.githubusercontent.com/Stepulin/general/master/package/openvpn/server_setup.sh && bash server_setup.sh
             ;;
         *) echo "invalid option $REPLY";;
     esac
