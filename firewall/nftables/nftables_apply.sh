bc45befe17efa2fddcb91c8e9b56e7320967da99
Stepulin <35203513+Stepulin@users.noreply.github.com>
2021-08-22 22:17:01 +0200

    Update nftables_apply.sh

--- firewall/nftables/nftables_apply.sh
+++ firewall/nftables/nftables_apply.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+(
+# Making sure that _nftables_ package is already installed
+apt install nftables
+systemctl enable nftables
+systemctl start nftables
+
+# Backup the current configuration
+cp /etc/nftables.conf /root/nftables.conf.backup.$(date +%Y-%m-%d_%H-%M-%S)
+
+# Download and copy own config that was downloaded
+wget ### && cp /root/nftables.conf /etc/nftables.conf
+
+# Apply the rules
+nft -f /etc/nftables.conf && clear && nft list ruleset
+) | tee -a /root/log
