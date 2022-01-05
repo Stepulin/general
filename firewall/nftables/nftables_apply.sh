5ae0ef0a333ae9de608d48d9ad800d531f7fe68b
Stepulin <35203513+Stepulin@users.noreply.github.com>
2022-01-05 21:59:13 +0100

    Update nftables_apply.sh

--- firewall/nftables/nftables_apply.sh
+++ firewall/nftables/nftables_apply.sh
@@ -2,22 +2,14 @@
 # Declare the variables
 nft="/sbin/nft"
 
-# Making sure that _nftables_ package is already installed
-apt install nftables
-systemctl enable nftables
-systemctl start nftables
-
 # Backup the current configuration
 cp /etc/nftables.conf /root/nftables.conf.backup.$(date +%Y-%m-%d_%H-%M-%S)
 
-# Download and copy own config that was downloaded
-wget https://raw.githubusercontent.com/Stepulin/general/master/firewall/nftables/nftables.conf && cp /root/nftables.conf /etc/nftables.conf
+# Copy a new file from _root_ folder to _etc_
+cp /root/nftables.conf /etc/nftables.conf
 
 # Apply the rules
 ${nft} -f /etc/nftables.conf
-
-# Delete / purge
-apt-get purge iptables -y
 
 while true; do
     read -p "Do you want to CLEAR the terminal and LIST the current RULESET?" yn
